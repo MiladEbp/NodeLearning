c87c3b21210192dce64b2fa328c785da85f21d6b
milad <ebrahimpourmilad74@gmail.com>
2017-07-18 20:19:52 -0700

    upadate for session

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');// add Express in nodejs
 var router = express.Router();
 var bodyParser = require('body-parser');// call body-parser in  my project
-router.use(bodyParser.urlencoded({ extended: true }));
+var session = require('express-session');
 
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(session({
+    secret:'bbbbb'
+}));
 
 ////////////////////////////////////////////////////////START VASH//////////////////////////////////////////////////////
 
 router.get('/login', function (req, res) {// Insert value in Dynamic parameters in login page
+
 
     res.render('login', {
         title: 'LOGIN FORM (vash)',
@@ -14,21 +20,35 @@ router.get('/login', function (req, res) {// Insert value in Dynamic parameters 
         lab2:'Password : '
     });
 
+
+
 });
  router.post('/well',function(req,res){// Insert value in Dynamic parameters in well page and post value from login page for well page
-     res.render('well',{
-         title:"Well Come in Page ",
-         tit1:"Your UserName is  : ",
-         tit2:"Your Password is : ",
-         username:req.body.username,
-         password:req.body.pass
+    var userSession = req.session;
+     userSession.username = req.body.username;
+    if(userSession.username == 'milad'){
+        res.render('well',{
+            title:"Well Come in Page ",
+            tit1:"Your UserName is  : ",
+            tit2:"Your Password is : ",
+            username:userSession.username,
+            password:req.body.pass
 
-     });
+        });
+    }else{
+        console.log('faild login')
+    }
+
+
+
+
 
  });
 
  router.post('/login',function (req, res) {// return respond
+
      res.redirect('login');
+
  });
 
 ///////////////////////////////////////////////////////END VASH/////////////////////////////////////////////////////////
