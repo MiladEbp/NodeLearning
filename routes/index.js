600da7aed7ad0f039e14152bb07a142e255e9d09
milad <ebrahimpourmilad74@gmail.com>
2017-07-19 19:45:36 -0700

    update destroy

--- routes/index.js
+++ routes/index.js
@@ -25,18 +25,19 @@ router.get('/login', function (req, res) {// Insert value in Dynamic parameters 
 });
  router.post('/well',function(req,res){// Insert value in Dynamic parameters in well page and post value from login page for well page
     var userSession = req.session;
-     userSession.username = req.body.username;
+     userSession.username =  req.body.username;
     if(userSession.username == 'milad'){
         res.render('well',{
             title:"Well Come in Page ",
             tit1:"Your UserName is  : ",
             tit2:"Your Password is : ",
-            username:userSession.username,
-            password:req.body.pass
-
+            tit3:"Your Session is : ",
+            username:req.body.username,
+            password:req.body.pass,
+            session: userSession.username
         });
     }else{
-        console.log('faild login')
+        console.log('faild login');
     }
 
 
@@ -47,8 +48,13 @@ router.get('/login', function (req, res) {// Insert value in Dynamic parameters 
 
  router.post('/login',function (req, res) {// return respond
 
+    var out = req.session.destroy();
      res.redirect('login');
-
+    if(out){
+        console.log('Destroy session')
+    }else{
+        console.log(' dont destroy session')
+    }
  });
 
 ///////////////////////////////////////////////////////END VASH/////////////////////////////////////////////////////////
